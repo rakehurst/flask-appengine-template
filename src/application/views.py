ea04883eb463acf981bb5cddcf85180148d10cb3
rakehurst <rakehurst@bonline.com>
2013-07-17 18:02:02 +0100

    views

--- src/application/views.py
+++ src/application/views.py
@@ -27,22 +27,7 @@ cache = Cache(app)
 def home():
     """List all examples"""
     examples = ExampleModel.query()
-    form = ExampleForm()
-    if form.validate_on_submit():
-        example = ExampleModel(
-            example_name = form.example_name.data,
-            example_description = form.example_description.data,
-            added_by = users.get_current_user()
-        )
-        try:
-            example.put()
-            example_id = example.key.id()
-            flash(u'Example %s successfully saved.' % example_id, 'success')
-            return redirect(url_for('list_examples'))
-        except CapabilityDisabledError:
-            flash(u'App Engine Datastore is currently in read-only mode.', 'info')
-            return redirect(url_for('list_examples'))
-    return render_template('home.html', examples=examples, form=form)
+    return render_template('home.html', examples=examples)
 
 
 def say_hello(username):
